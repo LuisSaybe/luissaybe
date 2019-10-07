49bec69331e9e61c2cb3c5ce89db93ba81bf7155
Luis Banegas Saybe <luis@trois.io>
2019-10-07 19:46:13 +0200

    fix(travis)

--- docker/docker-push.bash
+++ docker/docker-push.bash
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-if [ $TRAVIS_BRANCH == "master" ]
-then
-  envsubst < server/settings-production.js >  server/settings.js
-  envsubst < web/settings-production.js >  web/settings.js
-fi
-
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker build -t luissaybe/ttmik .
+docker build --rm -f docker/Dockerfile -t luissaybe/ttmik .
 docker push luissaybe/ttmik:latest
 docker logout
