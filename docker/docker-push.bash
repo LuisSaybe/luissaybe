b2bbf32c6888d8e928ce5921b531cbffe0333a87
Luis E. Banegas Saybe <luis@trois.io>
2020-03-28 15:01:09 -0400

    chore

--- docker/docker-push.bash
+++ docker/docker-push.bash
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-if [ $TRAVIS_BRANCH == "master" ]
-then
-    echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-    docker build --rm -f docker/Dockerfile -t luissaybe/luissaybe .
-    docker push luissaybe/luissaybe:latest
-    docker logout
-fi
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker push luissaybe/luissaybe:latest
+docker logout
