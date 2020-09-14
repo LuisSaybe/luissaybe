a2b35489082d1b116662bc7e85916088d82166de
Luis E. Banegas Saybe <luis@trois.io>
2020-09-14 16:07:24 -0400

    feat(music)

--- web/js/page/music/index.tsx
+++ web/js/page/music/index.tsx
@@ -5,13 +5,14 @@ import './style.scss';
 export function Music() {
   return (
     <div styleName='root'>
+      <a href='https://luissaybe.nyc3.digitaloceanspaces.com/music/peach_pie.mp3'>竹内まりや - Fushigi Na Peach Pie</a>
       <a href='https://luissaybe.nyc3.digitaloceanspaces.com/music/farewell.mp3'>트윈 폴리오 - 교별</a>
       <a href="https://luissaybe.nyc3.digitaloceanspaces.com/music/roy-kim-spring.mp3">로이킴 - 봄봄봄</a>
       <a href="https://luissaybe.nyc3.digitaloceanspaces.com/music/love_talkin.mp3">山下 達郎, Yamashita Tatsurō - Love Talkin</a>
       <a href="https://luissaybe.nyc3.digitaloceanspaces.com/music/ee-nar-chee.mp3">이날치 - 범 내려온다</a>
       <a href="https://luissaybe.nyc3.digitaloceanspaces.com/music/kim-slowly.mp3">김수영 - 시간아 천천히</a>
       <a href="https://luissaybe.nyc3.digitaloceanspaces.com/music/midnight_joke.mp3">間宮貴子 Takako Mamiya - Midnight Joke</a>
-      <a href="https://luissaybe.nyc3.digitaloceanspaces.com/music/peachpie.mp3">竹内まりや - Peach Pie</a>
+      <a href="https://luissaybe.nyc3.digitaloceanspaces.com/music/peachpie.mp3">竹内まりや - Fushigi Na Peach Pie (Live)</a>
       <a href="https://luissaybe.nyc3.digitaloceanspaces.com/music/rain.mp3">트윈 폴리오 - 빗속을 울며</a>
       <a href="https://luissaybe.nyc3.digitaloceanspaces.com/music/Twin%20Polio%20-%20Our%20Story.mp3">트윈 폴리오 - 우리 이야기</a>
       <a href="https://luissaybe.nyc3.digitaloceanspaces.com/music/tank.mp3">菅野 よう子, Kanno Yōko, シートベルツ - Tank!</a>
