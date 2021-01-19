f617b82c43e4a13aca770dbcb6d91e9d48fc916d
Luis E. Banegas Saybe <luis@trois.io>
2021-01-19 14:05:12 -0500

    ella

--- web/js/page/music/index.tsx
+++ web/js/page/music/index.tsx
@@ -25,6 +25,31 @@ export function Music() {
       <a href='https://luissaybe.nyc3.digitaloceanspaces.com/music/iris.mp3'>James Bartholomew - Iris</a>
       <a href='https://luissaybe.nyc3.digitaloceanspaces.com/music/wedding_cake.mp3'>트윈폴리오 - 위딩케잌</a>
       <a href='https://luissaybe.nyc3.digitaloceanspaces.com/music/disco.mp3'>뉴 디스코</a>
+      <a href='https://luissaybe.nyc3.digitaloceanspaces.com/music/ella/20%20Sing%20You%20Sinners.mp3'>Sing You Sinners</a>
+      <a href='https://luissaybe.nyc3.digitaloceanspaces.com/music/ella/22%20Peg%20O%27%20My%20Heart.mp3'>Peg O' My Heart</a>
+      <a href='https://luissaybe.nyc3.digitaloceanspaces.com/music/ella/13%20Sly%20Mongoose.mp3'>Sly Mongoose</a>
+      <a href='https://luissaybe.nyc3.digitaloceanspaces.com/music/ella/14%20Blue%20Skies.mp3'>Blue Skies</a>
+      <a href='https://luissaybe.nyc3.digitaloceanspaces.com/music/ella/03%20Everybody%20Rock.mp3'>Everybody Rock</a>
+      <a href='https://luissaybe.nyc3.digitaloceanspaces.com/music/ella/03%20Everybody%20Rock.mp3'>Everybody Rock</a>
+      <a href='https://luissaybe.nyc3.digitaloceanspaces.com/music/ella/01%20Sugarfoot%20Stomp.mp3'>Sugarfoot Stomp</a>
+      <a href='https://luissaybe.nyc3.digitaloceanspaces.com/music/ella/02%20What%20Is%20This%20Thing%20Called%20Love.mp3'>What is this thing called love</a>
+      <a href='https://luissaybe.nyc3.digitaloceanspaces.com/music/ella/04%20Southland%20Shuffle.mp3'>Southland Shuffle</a>
+      <a href='https://luissaybe.nyc3.digitaloceanspaces.com/music/ella/05%20The%20Japanese%20Sandman.mp3'>The Japanese Sandman</a>
+      <a href='https://luissaybe.nyc3.digitaloceanspaces.com/music/ella/06%20Can%27t%20We%20Be%20Friends.mp3'>Can't We Be Friends</a>
+      <a href='https://luissaybe.nyc3.digitaloceanspaces.com/music/ella/07%20Let%27s%20Get%20Together.mp3'>Let's Get Together</a>
+      <a href='https://luissaybe.nyc3.digitaloceanspaces.com/music/ella/08%20Digga%20Digga%20Doo.mp3'>Digga Digga Doo</a>
+      <a href='https://luissaybe.nyc3.digitaloceanspaces.com/music/ella/09%20Raggin%27%20The%20Scale.mp3'>Raggin the Scale</a>
+      <a href='https://luissaybe.nyc3.digitaloceanspaces.com/music/ella/10%20Copenhagen.mp3'>Copenhagen</a>
+      <a href='https://luissaybe.nyc3.digitaloceanspaces.com/music/ella/11%20I%27ve%20Found%20A%20New%20Baby.mp3'>I Found a New Baby</a>
+      <a href='https://luissaybe.nyc3.digitaloceanspaces.com/music/ella/12%20Blue%20Lou.mp3'>Blue Lou</a>
+      <a href='https://luissaybe.nyc3.digitaloceanspaces.com/music/ella/16%20Stompin%27%20At%20The%20Savoy.mp3'>Stompin at the Savoy</a>
+      <a href='https://luissaybe.nyc3.digitaloceanspaces.com/music/ella/15%20In%20The%20Groove%20%28At%20The%20Grove%29.mp3'>In the Groove (at the Groove)</a>
+      <a href='https://luissaybe.nyc3.digitaloceanspaces.com/music/ella/17%20The%20Harlem%20Stride.mp3'>The Harlem Stride</a>
+      <a href='https://luissaybe.nyc3.digitaloceanspaces.com/music/ella/18%20One%20O%27Clock%20Jump.mp3'>One O'Clock Jump</a>
+      <a href='https://luissaybe.nyc3.digitaloceanspaces.com/music/ella/19%20Jubilee%20Swing.mp3'>Jubilee Swing</a>
+      <a href='https://luissaybe.nyc3.digitaloceanspaces.com/music/ella/21%20After%20You%27ve%20Gone.mp3'>After You've Gone</a>
+      <a href='https://luissaybe.nyc3.digitaloceanspaces.com/music/ella/23%20Back%20Bay%20Shuffle.mp3'>Back Bay Shuffle</a>
+      <a href='https://luissaybe.nyc3.digitaloceanspaces.com/music/ella/24%20Undecided.mp3'>Undecided</a>
     </div>
   );
 }
